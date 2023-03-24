5f28c852851dfd17258f7b09fdc08f280e8ba836
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-25 01:27:40 +0530

    Attendence

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace EmpWageComputationProblemWithOOP
+﻿using EmployeeWageComputationProblem;
+
+namespace EmpWageComputationProblemWithOOP
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Problems with OOPS");
+            EmpWageForMultipleComponies Companies = new EmpWageForMultipleComponies();
+            Companies.computeEmpWage();
 
         }
     }
