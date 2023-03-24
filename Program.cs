a9ea93aefa4fffd4cee797b3b06a149e7f71cc3b
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-25 01:50:40 +0530

    Calculate Total

--- Program.cs
+++ Program.cs
@@ -7,9 +7,6 @@ namespace EmpWageComputationProblemWithOOP
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Problems with OOPS");
-            EmpWageForMultipleComponies Companies = new EmpWageForMultipleComponies();
-            Companies.computeEmpWage();
-
-        }
+           
     }
 }
