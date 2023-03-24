a4e4ec1cf35c80dd028f238dc60f7a11048e8775
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-25 01:20:15 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmpWageComputationProblemWithOOP
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Problems with OOPS");
+
+        }
+    }
+}
